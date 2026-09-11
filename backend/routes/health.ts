5414ae7969e704f73b79c9edfd4122fb0553eb28
JohaJM2 <j.jimenez.10@estudiantec.cr>
2026-09-11 16:24:04 -0600

    Agreguee el endpoint /health

---
+++ backend/routes/health.ts
@@ -0,0 +1,6 @@
+import {Router, type Request, type Response} from 'express';
+
+const router = Router();
+router.get('/', (req: Request, res: Response) => {res.status(200).json({status: 'OK'})});
+
+export default router;
