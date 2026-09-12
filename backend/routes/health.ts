4495f20cd3220d5c3084e06aa9f1c9d13a8f8a79
MR.ERIK <80484346+ErikZF@users.noreply.github.com>
2026-09-12 16:57:47 -0600

    feat: compose-dev de bd, backend, kaycloak y el dockerfile.dev (backend)

--- backend/routes/health.ts
+++ backend/routes/health.ts
@@ -1,6 +1,6 @@
 import {Router, type Request, type Response} from 'express';
 
-const router = Router();
-router.get('/', (req: Request, res: Response) => {res.status(200).json({status: 'OK'})});
+const healthRouter = Router();
+healthRouter.get('/', (req: Request, res: Response) => {res.status(200).json({status: 'OK'})});
 
-export default router;
+export default healthRouter;
