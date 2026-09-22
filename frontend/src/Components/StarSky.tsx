bd527eda278d0c88c69634931d563be7320069a0
Neithan <n.vargas.5@estudiantec.cr>
2026-09-22 10:47:32 -0600

    Fixed docker compose dev

--- frontend/src/Components/StarSky.tsx
+++ frontend/src/Components/StarSky.tsx
@@ -1,42 +1,40 @@
-import type { NuevaEstrella } from '../types';
-import '../App.css'
+import { useEffect, useState } from 'react';
+import type { Estrella } from '../types';
+import '../App.css';
+import { readEstrellas } from '../Api';
 
 function StarSky() {
+  const [estrellas, setEstrellas] = useState<Estrella[]>([]);
 
-  const total_estrellas = 10;
-  const estrellas: NuevaEstrella[] = [];
+  useEffect(() => {
+    const fetchEstrellas = async () => {
+      const data = await readEstrellas();
+      if (Array.isArray(data)) {
+        setEstrellas(data);
+      }
+    };
 
-  for (let i = 0; i < total_estrellas; i++) {
-    let nuevaEstrella: NuevaEstrella = { nombre:'pepe', masa: 0, color: 123312,cord_x: 0, cord_y: 0 };
-
-    nuevaEstrella.masa = Math.random() * 18 + 2; // tamaño de [2, 10]
-    nuevaEstrella.cord_x = Math.random() * 1024; // A esto hay que sumarle la posición anterior
-    nuevaEstrella.cord_y = Math.random() * 1024; // falta CAMBIAR EL 1O24 por height
-
-    estrellas.push(nuevaEstrella);
-  }
+    fetchEstrellas();
+  }, []);
 
   // TODO hacer un hook para calcular la posición, para reajustar resize de screen
   return (
-  <>
-    {estrellas.map((estrella, index) => (
-      <div
-        key={index}
-        className='estrella'
-        style={{
-          width: estrella.masa,
-          height: estrella.masa,
+    <>
+      {estrellas.map((estrella, index) => (
+        <div
+          key={estrella.id ?? index}
+          className='estrella'
+          style={{
+            width: estrella.masa,
+            height: estrella.masa,
 
-          left: estrella.cord_x,
-          top: estrella.cord_y
-        }}>
-
-      </div>
-    ))
-
-    }
-  </>
-  )
+            left: estrella.cord_x,
+            top: estrella.cord_y,
+          }}
+        />
+      ))}
+    </>
+  );
 }
 
 export default StarSky;
