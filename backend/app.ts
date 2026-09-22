b610bba1cdc7bf728baa16f7f7e57c1768c260f7
MR.ERIK <80484346+ErikZF@users.noreply.github.com>
2026-09-22 12:15:13 -0600

    feat: cors para la comunicacion entre frontend y backend, tambien borre el  env del deployment de frontend (ya esta en dockerfile por el nginx)

--- backend/app.ts
+++ backend/app.ts
@@ -1,9 +1,11 @@
 import express, { type Express, type Request, type Response } from 'express';
 import healthRouter from './routes/health.ts';
+import cors from 'cors';
 import readyRouter from './routes/ready.ts';
 import starRouter from './routes/star.ts';
 
 const app: Express = express();
+app.use(cors({ origin: process.env.FRONTEND_ORIGIN }));
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
